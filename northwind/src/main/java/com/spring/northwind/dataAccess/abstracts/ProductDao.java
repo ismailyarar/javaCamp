406b065a0b5c52d0b20a9dc60b3979c70547782e
ismya <ismyarar@gmail.com>
2021-05-08 23:27:06 +0300

    lombook update

---
+++ northwind/src/main/java/com/spring/northwind/dataAccess/abstracts/ProductDao.java
@@ -0,0 +1,11 @@
+package com.spring.northwind.dataAccess.abstracts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.spring.northwind.entities.concretes.Product;
+
+public interface ProductDao extends JpaRepository<Product,Integer>{
+	
+	
+
+}
