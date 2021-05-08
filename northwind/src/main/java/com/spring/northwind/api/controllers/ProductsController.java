0d36c387f2844bafe45acee12460944751cdaf39
ismya <ismyarar@gmail.com>
2021-05-08 23:48:03 +0300

    managerandcontroller

---
+++ northwind/src/main/java/com/spring/northwind/api/controllers/ProductsController.java
@@ -0,0 +1,13 @@
+package com.spring.northwind.api.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductsController {
+
+}
+
+
+//facebook.com/api/products
--- northwind/src/main/java/com/spring/northwind/business/concretes/ProductManager.java
+++ northwind/src/main/java/com/spring/northwind/business/concretes/ProductManager.java
@@ -2,10 +2,24 @@ package com.spring.northwind.business.concretes;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.spring.northwind.business.abstracts.ProductService;
+import com.spring.northwind.dataAccess.abstracts.ProductDao;
 import com.spring.northwind.entities.concretes.Product;
 
+@Service
 public class ProductManager implements ProductService {
+
+	//DI
+	private ProductDao productDao;
+	
+	@Autowired
+	public ProductManager(ProductDao productDao) {
+		super();
+		this.productDao = productDao;
+	}
 
 	@Override
 	public List<Product> getAll() {
